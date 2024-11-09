07dd585f439bb7aca3bc676756e0368244fe4f87
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2024-11-10 00:53:58 +0530

    This is my spring employee management system
    This is my spring employee management system

---
+++ src/main/java/com/murali/emp/mgmt/system/MuraliEmpMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.murali.emp.mgmt.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuraliEmpMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuraliEmpMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murali/emp/mgmt/system/MuraliEmpMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murali.emp.mgmt.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuraliEmpMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
